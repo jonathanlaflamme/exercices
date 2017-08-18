ff600ac24eb970dfc890297fbfaea2d6bd6be6a0
Jonathan Laflamme <jonathan.laflamme.jl@gmail.com>
2017-08-18 16:05:05 -0400

    node/wesbos

--- NodeJS/wes_bos/dang-its-delicious/routes/index.js
+++ NodeJS/wes_bos/dang-its-delicious/routes/index.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController')
 
 // Do work here
-router.get('/', (req, res) => {
-  const wes = { name: 'Wes', age: 100, cool: true};
-  //res.send('Hey! It works!');
-  res.json(wes);
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-})
+router.get('/', storeController.homePge);
 
 module.exports = router;
